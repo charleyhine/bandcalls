753ce89851c2731a36f48369f0fe212755fe7a67
Charley Hine <charles.hine@gmail.com>
2012-09-02 01:59:53 -0700

    bootstrapped up call scaffold views

--- app/models/artist_call.rb
+++ app/models/artist_call.rb
@@ -1,6 +1,6 @@
 class ArtistCall < ActiveRecord::Base
   # attr_accessible :title, :body
   has_one :user_request
-  has_one artist, :through => :user_request
-  has_one user, :through => :user_request
+  has_one :artist, :through => :user_request
+  has_one :user, :through => :user_request
 end
