3eed08e4c9bbd3f6eee788c5da08183d90dd11c9
Charley Hine <charles.hine@gmail.com>
2012-09-01 20:25:23 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandcalls::Application.config.secret_token = '5bd9996c41acf9cee2780edf9c7bb606ba36f344489003dab1297129e13257e7ac2a43ca09d4a10119b8a61bb37f09252ef6d682eb83ef2419e08d34863bc2c1'
