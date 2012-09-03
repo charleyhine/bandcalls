e90ef7c55a370a08f513b4efd86918dd18d306e4
Charley Hine <charles.hine@gmail.com>
2012-09-02 23:36:43 -0700

    added base url bandcalls.com

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
-BASE_URL = 'http://bandcalls.herokuapp.com'
+BASE_URL = 'http://bandcalls.com'
 
 TWILIO_ACCOUNT_SID = 'AC0e58f3b4678142a19691cd5ff4321012'
 TWILIO_AUTH_TOKEN = '11ae194d01ee29eb66f80114e54650b2'
