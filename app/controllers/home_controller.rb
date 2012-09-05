b3ea42d6d13d45d5171db2a1074019e78781e547
Charley Hine <charles.hine@gmail.com>
2012-09-04 21:01:25 -0700

    added user / admin logout logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
-
+    if current_user && current_artist
+      sign_out :user
+    end
+    
+    if current_user
+      redirect_to artists_path
+    end
+    
+    if current_artist
+      redirect_to artist_requests_path
+    end
   end
 end
